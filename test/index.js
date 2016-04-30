c0485fbe926eb413c8052fe73938e09ce08ed140
Marco Moschettini <m.marmos@gmail.com>
2016-04-30 14:15:51 +0200

    Added sinon and chai packages for testing.

--- test/index.js
+++ test/index.js
@@ -1,9 +1,19 @@
 'use strict';
 
+var chai = require('chai');
+var sinon = require('sinon');
+var needle = require('needle');
+var sinon_chai = require('sinon-chai');
+var expect = chai.expect;
+
+chai.use(sinon_chai);
+
 var nappy = require('../src/index.js');
 
 describe('wait', function()
 {
+
+
   it('should wait for 1 second', function(done)
   {
     this.timeout(1500);
@@ -17,12 +27,34 @@ describe('wait', function()
     return nappy.wait.till(now).then(done);
   });
 
-  it('should wait for connection');
+  before(function()
+  {
+    sinon.stub(needle, 'get', function(url, callback)
+    {
+      var ip = '2.224.212.173';
+      callback(undefined, {statusCode: 200, body: ip});
+    });
+  });
+
+  it('should wait for connection', function(done)
+  {
+    nappy.wait.connection().then(done);
+  });
+
+  after(function()
+  {
+    needle.get.restore();
+  });
 });
 
 describe('alarm', function()
 {
-  it('constructor must be called with new');
+  it('constructor must be called with new', function()
+  {
+    var constructor_exception = {code: 0, description: 'Constructor must be called with new.', url: ''};
+    return expect(nappy.alarm).to.throw(constructor_exception);
+  });
+
   it('should be reset');
   it('should abort');
   it('should throw already went off exception');
